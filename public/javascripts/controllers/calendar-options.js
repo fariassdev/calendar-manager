f3366291e42ed1b5d07493670c47ffc5480d8f8f
Fernando Arias Santos <fernan.fpinformatica@gmail.com>
2016-06-20 02:13:15 +0200

    Final commit

--- public/javascripts/controllers/calendar-options.js
+++ public/javascripts/controllers/calendar-options.js
@@ -1,12 +1,12 @@
 myApp.controller('calendar-options', ['$scope',  function($scope) {
-    // Dates can be passed as strings or Date objects
+
     $scope.calendarOptions = {
         defaultDate: new Date(),
         minDate: new Date([2015, 1, 1]),
         maxDate: new Date([2020, 12, 31]),
-        dayNamesLength: 9, // How to display weekdays (1 for "L", 2 for "Lu", 3 for "Lun"; 9 will show full day names; default is 9)
-        multiEventDates: true, // Set the calendar to render multiple events in the same day or only one event, default is false
-        maxEventsPerDay: 1, // Set how many events should the calendar display before showing the 'More Events' message, default is 3;
+        dayNamesLength: 9,
+        multiEventDates: true,
+        maxEventsPerDay: 1,
         eventClick: $scope.eventClick,
         dateClick: $scope.dateClick
     };
